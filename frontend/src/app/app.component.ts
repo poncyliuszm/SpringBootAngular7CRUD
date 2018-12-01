6038f6906c3b8696ca06883e741f1a3ab8f8b4bc
mateuszp <mateuszp@meritus.pl>
2018-12-01 15:05:24 +0100

    front lista i update

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  customers = [];
+  customerOrders = [];
+  displayedColumns: string[] = ['firstName', 'lastName', 'email'];
+  janName: any;
+
+
+  constructor(private http: HttpClient){
+
+  }
+
+  ngOnInit() {
+    this.getCustomers();
+  }
+
+  save() {
+    let customer = {
+      "firstName": "imieTest",
+      "secondName": "nazwTEst",
+      "email": "email@test"
+    };
+    this.http.post("http://localhost:8080/customer/save", customer).subscribe((data: any) => {
+      console.log("saved");
+    })
+  }
+
+  updateJan() {
+    console.log('update start');
+    let janId :number = 1;
+    this.http.get("http://localhost:8080/customer/" + janId).subscribe((data: any) => {
+      let customer = data;
+      customer['firstName'] = this.janName;
+      this.http.put("http://localhost:8080/customer/update", customer).subscribe((data: any) => {
+        console.log("updated");
+        this.getCustomers();
+      })
+    });
+  }
+
+  delete() {
+    let customerId = 1;
+
+    this.http.delete("http://localhost:8080/customer/delete/" + customerId).subscribe((data: any) => {
+      console.log("deleted");
+    })
+  }
+
+  getCustomers() {
+    this.http.get("http://localhost:8080/customer/list").subscribe((data: any) => {
+      this.customers = data;
+    });
+  }
+
 }
+
+
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {MatButtonModule, MatFormFieldModule, MatInputModule, MatTableModule, MatTabsModule} from "@angular/material";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatTabsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatButtonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
